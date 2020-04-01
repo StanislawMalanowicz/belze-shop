97e73d6681734df2b865d78c251e17023d39438a
Your Name <you@example.com>
2020-04-01 19:26:40 +0200

    adress form added

--- js/order.js
+++ js/order.js
@@ -2,10 +2,11 @@ const orderButton = document.querySelector('.order-button');
 
 orderButton.addEventListener('click', (e) => {
     e.preventDefault();
+    
     const items = document.querySelectorAll('.itemRow');
-    // items.map(el => el.innerHTML)
     const list = [...items];
-    const activeOrder = list.map(el => el.innerText.split(/\t/).filter(Boolean).join(" "))
+    const activeOrder = list.map(el => el.innerText.split(/\t/).filter(Boolean))
+    const 
     console.log('click order: ', activeOrder);  
 
 
